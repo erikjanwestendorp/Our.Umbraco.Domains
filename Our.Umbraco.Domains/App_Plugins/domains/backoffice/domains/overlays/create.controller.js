cce718a47b2d28fcc849f784d12c0553b2f20deb
Callum Whyte <hey@callumwhyte.com>
2020-10-07 13:07:32 +0100

    Adding content picker to create dialog

--- Our.Umbraco.Domains/App_Plugins/domains/backoffice/domains/overlays/create.controller.js
+++ Our.Umbraco.Domains/App_Plugins/domains/backoffice/domains/overlays/create.controller.js
@@ -1,11 +1,32 @@
 ﻿(function () {
     'use strict';
 
-    function CreateDomainController() {
+    function CreateDomainController($scope, languageResource, contentResource) {
 
         var vm = this;
 
         vm.hostname = null;
+        vm.node = null;
+        vm.dialogTreeApi = {};
+
+        vm.onTreeInit = function () {
+            vm.dialogTreeApi.callbacks.treeNodeSelect(nodeSelectHandler);
+        };
+
+        function nodeSelectHandler(args) {
+            if (args && args.event) {
+                args.event.preventDefault();
+                args.event.stopPropagation();
+            }
+
+            if (vm.node) {
+                //un-select if there's a current one selected
+                vm.node.selected = false;
+            }
+
+            vm.node = args.node;
+            vm.node.selected = true;
+        };
     }
 
     angular.module('umbraco')
