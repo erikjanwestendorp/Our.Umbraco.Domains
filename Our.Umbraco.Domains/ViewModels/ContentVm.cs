991b5999da648c1eafa6946f70ebbe313a064142
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2022-01-09 20:00:35 +0100

    Add DataContract and DataMember to ViewModels

--- Our.Umbraco.Domains/ViewModels/ContentVm.cs
+++ Our.Umbraco.Domains/ViewModels/ContentVm.cs
@@ -1,9 +1,14 @@
-﻿namespace Our.Umbraco.Domains.ViewModels
+﻿using System.Runtime.Serialization;
+
+namespace Our.Umbraco.Domains.ViewModels
 {
+    [DataContract(Name = "content", Namespace = "")]
     public class ContentVm
     {
+        [DataMember(Name = "id")]
         public int Id { get; set; }
 
+        [DataMember(Name = "name")]
         public string Name { get; set; }
     }
 }
--- Our.Umbraco.Domains/ViewModels/DomainVm.cs
+++ Our.Umbraco.Domains/ViewModels/DomainVm.cs
@@ -1,15 +1,23 @@
-﻿namespace Our.Umbraco.Domains.ViewModels
+﻿using System.Runtime.Serialization;
+
+namespace Our.Umbraco.Domains.ViewModels
 {
+    [DataContract(Name = "domain", Namespace = "")]
     public class DomainVm
     {
+        [DataMember(Name = "id")]
         public int Id { get; set; }
 
+        [DataMember(Name = "domainName")]
         public string DomainName { get; set; }
 
+        [DataMember(Name = "rootContentId")]
         public int? RootContentId { get; set; }
 
+        [DataMember(Name = "content")]
         public ContentVm Content { get; set; }
 
+        [DataMember(Name = "languageIsoCode")]
         public string LanguageIsoCode { get; set; }
     }
 }
